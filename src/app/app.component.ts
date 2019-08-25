297a8fc0478be41b2a545e0497b0ce25306264e9
Piyush Bhardwaj <Piyush_Bhardwaj@ad.infosys.com>
2019-08-25 16:56:58 +0530

    order tile filtering and sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,37 @@ import { OrdersDataService } from './orders-data.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'orders';
+  title = 'Rewardr - Orders';
+  orderTilesData = [];
 
   constructor(private ordersDataService: OrdersDataService) { }
 
   ngOnInit() {
-    this.ordersDataService.getOrders().subscribe((data: any) => {
+    this.fetchOrderDetails();
+  }
+
+  fetchOrderDetails() {
+    this.ordersDataService.getOrders().subscribe((response) => this.populateOrderTiles(response));
+  }
+
+  populateOrderTiles(ordersData) {
+    //status
+    console.log(ordersData);
+    ordersData = ordersData.filter(order => order.status != 'cancelled').sort(function(a, b){
+      let x = a.status;
+      let y = b.status;
+      if (x === 'processing' && y === 'completed') {return -1;}
+      if (x === 'completed' && y === 'processing') {return 1;}
+      return 0;
+    });;
+    console.log(ordersData);
+    this.orderTilesData = ordersData;
+  }
+
+  updateOrder(orderNo){
+    this.ordersDataService.updateOrder(orderNo).subscribe((data: any) => {
+      this.fetchOrderDetails();
+      console.log('update order - success!');
       console.log(data);
     });
   }
--- src/app/orders-data.service.ts
+++ src/app/orders-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +8,30 @@ export class OrdersDataService {
 
   constructor(private http: HttpClient) { }
 
-  private _url = 'https://rewardr.in/wp-json/wc/v3/orders?consumer_key=ck_5a2e407a2feb83a21cfc71f34f2039cb865cd235&consumer_secret=cs_315fc9c098f10c13402691b195c2b820aee4d6d3';
+  private _key = 'ck_5a2e407a2feb83a21cfc71f34f2039cb865cd235';
+  private _secret = 'cs_315fc9c098f10c13402691b195c2b820aee4d6d3'
+  private _url = `https://rewardr.in/wp-json/wc/v3/orders`;
 
   getOrders(){
-    return this.http.get(this._url);
+    let listOrdersUrl = `${this._url}?consumer_key=${this._key}&consumer_secret=${this._secret}`
+    return this.http.get(listOrdersUrl);
+  }
+
+  updateOrder(orderId){
+    let updateOrderUrl = `${this._url}/${orderId}?consumer_key=${this._key}&consumer_secret=${this._secret}`;
+
+    const request = {
+      'status': 'completed'
+    };
+    
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',        
+      })
+    };
+
+
+
+    return this.http.put(updateOrderUrl, request, httpOptions);
   }
 }
