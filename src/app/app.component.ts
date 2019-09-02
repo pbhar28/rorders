b8f23c59ed81523fb0d42da978c4c2c508fc6c6d
Piyush Bhardwaj <Piyush_Bhardwaj@ad.infosys.com>
2019-09-03 00:45:47 +0530

    Changes to calculated order status based on created_time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,16 +20,35 @@ export class AppComponent implements OnInit {
     this.ordersDataService.getOrders().subscribe((response) => this.populateOrderTiles(response));
   }
 
+  calculateOrderTime(status, respDateCreated) {
+    if (status != 'completed') {
+      respDateCreated = `${respDateCreated}+05:30`;
+      let dateCreated = new Date(Date.parse(respDateCreated));
+      let currentDate = new Date();
+      let diff = parseInt((currentDate - dateCreated)/60000);
+  
+      if(diff < 10) return 'normal'
+      else if (diff >= 10 && diff < 15) return 'on-time'
+      else return 'delayed';
+    } else return 'completed';
+  }
+
   populateOrderTiles(ordersData) {
     //status
     console.log(ordersData);
     ordersData = ordersData.filter(order => order.status != 'cancelled').sort(function(a, b){
       let x = a.status;
       let y = b.status;
-      if (x === 'processing' && y === 'completed') {return -1;}
-      if (x === 'completed' && y === 'processing') {return 1;}
+      if (x === 'processing' && y === 'completed') return -1;
+      if (x === 'completed' && y === 'processing') return 1;
       return 0;
-    }); //date_created "2019-08-26T14:01:35"
+    }).map(order => {
+      let customStatus = this.calculateOrderTime(order.status, order.date_created);
+      return { customStatus, ...order }
+    });
+    
+    //date_created "2019-08-26T14:01:35"
+
     console.log(ordersData);
     this.orderTilesData = ordersData;
   }
